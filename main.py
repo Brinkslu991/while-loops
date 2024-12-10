ceb788c1b3328d9ead1594d1b178865f8b610f7a
Brinkslu991 <153300807+Brinkslu991@users.noreply.github.com>
2024-12-10 11:04:16 -0500

    added an anticrash

--- main.py
+++ main.py
@@ -3,24 +3,29 @@
 # while loops
 
 # Movie Tickets
-# while True:
-#     age = int(input('Enter your age (put in -99 to quit):'))
-#     if age == -99:
-#         break
-#     elif age < 3:
-#         print('You get your ticket for free small person.')
-#     elif age <= 12:
-#         print('Your ticket costs $10.')
-#     else:
-#         print('Your ticket cost $15.')
+while True:
+    try:
+        age = int(input('Enter your age (put in -99 to quit):'))
+        if age == -99:
+            break
+        elif age < 3:
+            print('You get your ticket for free small person.')
+        elif age <= 12:
+            print('Your ticket costs $10.')
+        else:
+            print('Your ticket cost $15.')
+    except ValueError:
+        print('Please enter in a number only.')
 
 # Three Exits
 people = []
 while len(people) < 7:
-    age = int(input('Enter your age (put in -99 to quit):'))
-    if age == -99:
-        break
-    else:
-        people.append(age)
-
+    try:
+        age = int(input('Enter your age (put in -99 to quit):'))
+        if age == -99:
+          break
+        else:
+            people.append(age)
+    except ValueError:
+        print('Please enter in a number only')
 print(f'These ages bought tickets: {people}')
